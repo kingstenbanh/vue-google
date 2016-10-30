c84cb3461e7b84638817a2ba80e01dee445a0068
Kingsten Banh <kingstenbanh@gmail.com>
2016-10-30 01:41:33 -0700

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <header class="kb-header">
+    <slot></slot>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  components: {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .kb-header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;;
+  }
+</style>
