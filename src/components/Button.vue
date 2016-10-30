8de80fcdb3c2374b9ec13ac117a6c0d59f549732
Kingsten Banh <kingstenbanh@gmail.com>
2016-10-29 23:53:07 -0700

    add a button component with 2 button types

---
+++ src/components/Button.vue
@@ -0,0 +1,73 @@
+<template lang="">
+  <button
+    type="button"
+    name="button"
+    class="kb-button"
+    :class="classNames">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'button',
+  components: {
+
+  },
+  props: ['text', 'type'],
+  computed: {
+    classNames() {
+      return {
+        'kb-button--primary': this.type === 'primary',
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  button {
+    &:hover,
+    &:active,
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .kb-button {
+    background-image: -webkit-linear-gradient(top, #f5f5f5, #f1f1f1);
+    background-color: #f2f2f2;
+    border: 1px solid #f2f2f2;
+    border-radius: 2px;
+    color: #757575;
+    font-size: 13px;
+    font-weight: bold;
+    height: 36px;
+    line-height: 27px;
+    margin: 11px 4px;
+    min-width: 54px;
+    padding: 0 16px;
+    text-align: center;
+
+    &:hover {
+      background-image: -webkit-linear-gradient(top,#f8f8f8,#f1f1f1);
+      background-color: #f8f8f8;
+      border-color: #c6c6c6;
+      color: #222;
+    }
+
+    &--primary {
+      background-image: none;
+      background-color: #4285f4;
+      border-color: #4285f4;
+      color: #fff;
+
+      &:hover {
+        background-image: none;
+        background-color: #4285f4;
+        border-color: #4285f4;
+        color: #fff;
+      }
+    }
+  }
+</style>
